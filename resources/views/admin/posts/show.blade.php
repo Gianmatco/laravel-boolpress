605097448f5622055d7ff9b30872159e921c78e4
gianmarco <gianmarco.culqui.ikaros@gmail.com>
2022-06-29 17:09:34 +0200

    up

---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.admin')
+
+
+@section('content')
+
+    <h1>{{$post->title}}</h1>
+    <h2>{{$post->category->name}}</h2>
+    <small>{{$post->created_at}}</small>
+    <p>{{$post->content}}</p>
+    <h5>pubblicato:{{$post->published}}</h5>
+
+@endsection
